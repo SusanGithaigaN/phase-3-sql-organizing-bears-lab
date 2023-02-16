7225cc60cd5f8957dc8db05fa00bed3a4e52defa
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-02-16 07:11:39 +0300

    First commit

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+-- booleans 0:FALSE, 1 TRUE
+INSERT INTO bears VALUES 
+    (1,'Mr. Chocolate', 28, 'M', 'White', 'Social',0),
+    (2,'Rowdy', 17, 'M','Asian', 'irritable', 1),
+    (3,'Tabitha',22,'F','Black', 'Relaxed', 1),
+    (4,'Sergeant Brown',36,'F','Mexican', 'fire', 0),
+    (5,'Melissa',23,'F','Black','Optimistic',1),
+    (6,'Grinch',15,'M','Green','Irritable',1),
+    (7,'Wendy',32,'F','Russian','phlegmatic',1),
+    (8,NULL, 12,'F','Indian','fire',0);
